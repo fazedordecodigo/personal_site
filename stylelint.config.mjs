154c2947b9e851009ddd73c682fddd7afee74dd3
Emerson Delatorre <38289677+fazedordecodigo@users.noreply.github.com>
2026-08-15 12:46:21 -0300

    fix: align stylelint with site css

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,7 +1,11 @@
 export default {
   extends: ["stylelint-config-standard"],
   rules: {
+    "color-hex-length": null,
     "declaration-block-no-redundant-longhand-properties": null,
+    "media-feature-range-notation": null,
+    "no-descending-specificity": null,
+    "property-no-deprecated": null,
     "selector-class-pattern": null,
   },
 };
